9a7822fb164798af6d9642cf99bae8dc57241ca3
Andrey.Krichun <andrey.krichun@gmail.com>
2014-12-04 18:07:14 +0200

    first

---
+++ src/main/java/learn/ray/App.java
@@ -0,0 +1,13 @@
+package learn.ray;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Здравствуй, дорогой друг!" );
+    }
+}
