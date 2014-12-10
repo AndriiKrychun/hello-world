eea55b7f15877ed60ca8e78da7f5643ef7c816d2
Andrey.Krichun <andrey.krichun@gmail.com>
2014-12-10 17:11:18 +0200

    Task 'Calculator'

---
+++ src/main/java/learn/ray/Calculator.java
@@ -0,0 +1,49 @@
+package learn.ray;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        int sum=0;
+        int result=0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter expression : ");
+        String input = scanner.next();
+
+        Pattern pat = Pattern.compile("(\\d+)([\\+\\-\\/\\*])(\\d+)");
+        Matcher mat = pat.matcher(input);
+        mat.find();
+        String oper1 = mat.group(1);
+        String operator = mat.group(2);
+        String oper2 = mat.group(3);
+
+        //Scanner sc = new Scanner(input);
+        //sc.useDelimiter("[+-]");
+        //String oper1 = sc.next();
+        //String oper2 = sc.next();
+
+        switch (operator)
+        {
+            case "+":
+                result = Integer.valueOf(oper1) + Integer.valueOf(oper2);
+                break;
+            case "-":
+                result = Integer.valueOf(oper1) - Integer.valueOf(oper2);
+                break;
+            case "*":
+                result = Integer.valueOf(oper1) * Integer.valueOf(oper2);
+                break;
+            case "/":
+                result = Integer.valueOf(oper1) / Integer.valueOf(oper2);
+                break;
+
+        }
+        System.out.println(Integer.toString(result));
+
+    }
+
+}
+
