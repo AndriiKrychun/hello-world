5b54a20b87e2c53e7e2d0a6cec083370832e0c4f
Andrey.Krichun <andrey.krichun@gmail.com>
2014-12-10 17:20:47 +0200

    Task 'SumFromStringVar2'

---
+++ src/main/java/learn/ray/SumFromStringVar2.java
@@ -0,0 +1,21 @@
+package learn.ray;
+
+import java.util.Scanner;
+
+public class SumFromStringVar2 {
+
+    public static void main(String[] args) {
+        int sum=0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter numbers split by \';\' : ");
+        String input = scanner.next();
+        String numbers[] = input.split("\\;");
+        for (int i=0; i<numbers.length;i++)
+        {
+            sum += Integer.valueOf(numbers[i]);
+        }
+        System.out.println(Integer.toString(sum));
+
+    }
+
+}
