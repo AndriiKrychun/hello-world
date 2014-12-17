d5aeb6e4e8e0198cd1a0f7f7f4c7530d682a76fb
Andrey.Krichun <andrey.krichun@gmail.com>
2014-12-17 14:50:07 +0200

    Test for SumFromString is ready

--- src/main/java/learn/ray/SumFromString.java
+++ src/main/java/learn/ray/SumFromString.java
@@ -1,20 +1,27 @@
 package learn.ray;
 
+import tasks.StringSumAlgorithm;
+
 import java.util.Scanner;
 
-public class SumFromString {
+public class SumFromString implements StringSumAlgorithm {
 
     public static void main(String[] args) {
-        int sum = 0;
+        SumFromString sumFromString = new SumFromString();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter numbers split by some letters : ");
         String input = scanner.next();
-        String numbers[] = input.split("\\D+");
-        for (int i = 0; i < numbers.length; i++) {
-            sum += Integer.valueOf(numbers[i]);
-        }
-        System.out.println(Integer.toString(sum));
-
+        double sum = sumFromString.getSumFromString(input);
+        System.out.println(sum);
     }
 
+    @Override
+    public double getSumFromString(String input) {
+        double sum = 0;
+        String numbers[] = input.split("\\D+");
+        for (String i : numbers) {
+            sum += Integer.valueOf(i);
+        }
+        return sum;
+    }
 }
---
+++ src/main/java/learn/ray/SumFromStringTest.java
@@ -0,0 +1,23 @@
+package learn.ray;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class SumFromStringTest {
+
+    SumFromString sumFromString;
+
+    @Before
+    public void setUp() throws Exception {
+        sumFromString = new SumFromString();
+    }
+
+    @Test
+    public void testIsLeapYear() throws Exception {
+        double expectedSum = 8.0;
+        double actualSum = sumFromString.getSumFromString("1i3lh4");
+        Assert.assertEquals(expectedSum, actualSum, 0.01);
+    }
+}
